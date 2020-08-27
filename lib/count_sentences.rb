17725eb42da0b900b8a73757fbdf590a1175f0e9
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-27 23:53:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
+  self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+  # puts self
+  # puts self.split("/\?|\.|!/")
+  # puts self.count
+   puts self 
+   array = self.split(/[.?!]/)
+   return array.count
   end
 end
